faaaa27b11741b9a15c56a123ae09a6d497d2aaf
zhangna <15147851661@139.com>
2022-03-15 19:56:41 +0800

    冒泡，快排

--- src/排序算法/Sort.java
+++ src/排序算法/Sort.java
@@ -60,26 +60,85 @@ public class Sort {
 
     /**
      * 冒泡排序
-     * 思路：两两比较相邻记录的关键字，若反序则交换，直到没有反序记录为止
+     * 思路：两两比较相邻记录的关键字，若反序则交换，直到没有反序记录为止；
+     * 两个优化操作：1设置flag记录是否交换数据；2进行处理的数据每次从0到arr.length - 1 - i
      *
      * @param arr
      */
     public static void bubbleSort(int[] arr) {
 
-        //n个数据用冒泡走n-1趟即可有序，所以i从0~n-2
+        //n个数据用冒泡走n-1趟即可有序，所以i从0~n-2,i代表第几趟
         for (int i = 0; i < arr.length - 1; i++) {
 
             //flag用来判断本趟是否进行了交换
             boolean flag = false;
+            //j指向要比较交换的数，若是第3趟，说明最后三个数已有序，所以j从0~ arr.length - 1 - i
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    flag = true;
+                }
+            }
+            //若本趟没有交换数据，说明已经全部有序，即可跳出循环
+            if (!flag) {
+                break;
+            }
 
         }
     }
 
+    /**
+     * 快速排序
+     * 核心思想：---------怎么处理能让基准数左边都比他小，右边都比他大 （由这个要注意不是左右指针比大小交换，而是左右指针要和基准数比大小然后左右指针交换）
+     * 思路：
+     * 1.先把数组中的一个数当作基准数（一般会把数组中最左边的数当作基准数）
+     * 2.然后利用左右指针从两边进行检索。先从右边检索比基准数小的数，再从左边检索比基准数大的数。若左右指针都检索到了，就停下，然后交换左右指针所指的数。然后再继续检索
+     * 3.当左右指针相遇时，就停止检索。把基准数位置的数据和指针相遇位置的数据交换。--------此时，一趟结束后，基准数左边都比他小，右边都比他大
+     * 4.然后，对基准数的左右序列，再递归分别进行快排
+     */
+    public static void quickSort(int[] arr, int left, int right) {
+
+        //左指针>右指针时不合法，递归结束
+        if (left > right) {
+            return;
+        }
+
+        //base代表基准数，选取第一个记录为轴值
+        int base = arr[left];
+        int i = left, j = right;
+        while (i < j) {
+            //先看右指针，与base比较，依次往左递减，当arr[j]<base时跳出循环
+            while (i < j && arr[j] >= base) {
+                j--;
+            }
+            while (i < j && arr[i] <= base) {
+                i++;
+            }
+            //当前状态下，arr[j]<base，arr[i]>base，然后交换arr[j]与arr[i]的值
+            int temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+        }
+        //循环结束时，左右指针相遇，交换基准数位置与相遇位置的元素
+        arr[left] = arr[i];
+        arr[i] = base;
+
+        //至此，一趟结束，基准数左边都比他小，右边都比他大
+        //然后对由基准数分割的左右两边，再次使用快排
+        quickSort(arr, left, i - 1);
+        quickSort(arr, i + 1, right);
+    }
+
+
     public static void main(String[] args) {
         int[] arr = {49, 38, 65, 97, 23, 22, 76, 1, 5, 8, 2, 0, -1, 22};
         //insertSort(arr);
-        shellSort(arr);
-
+        //shellSort(arr);
+        //bubbleSort(arr);
+        quickSort(arr, 0, arr.length - 1);
         System.out.println("排序后:");
         for (int i : arr) {
             System.out.print(i + " ");
